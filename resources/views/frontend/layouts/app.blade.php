128f8742c8d536849355f38d52a911a9004b8df0
darshan45672 <drshnbhandary@gmail.com>
2025-05-21 12:46:16 +0530

    done navbar

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1,44 +1,36 @@
 <!DOCTYPE html><html lang="en" data-bs-theme="light" data-pwa="true">
-<!-- Mirrored from finder-html.createx.studio/home-city-guide.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 21 May 2025 06:22:10 GMT -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
 <head>
     <meta charset="utf-8">
 
     <!-- Viewport -->
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover">
 
-    <!-- SEO Meta Tags -->
     <title>@yield('title', 'Travel Tech')</title>
     <meta name="description" content="Finder - Directory &amp; Listings Bootstrap HTML Template">
     <meta name="keywords" content="directory, listings, search, car dealer, real estate, city guide, business listings, medical directories, event listings, e-commerce, market, multipurpose, ui kit, light and dark mode, bootstrap, html5, css3, javascript, gallery, slider, mobile, pwa">
     <meta name="author" content="Createx Studio">
 
-    <!-- Webmanifest + Favicon / App icons -->
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <link rel="manifest" href="manifest.json">
     <link rel="icon" type="image/png" href="{{ asset('assets/app-icons/icon-32x32.png') }}" sizes="32x32">
     <link rel="apple-touch-icon" href="{{ asset('assets/app-icons/icon-180x180.png') }}">
 
-    <!-- Theme switcher (color modes) -->
     <script src="assets/js/theme-switcher.js"></script>
 
-    <!-- Preloaded local web font (Inter) -->
     <link rel="preload" href="{{ asset('assets/fonts/inter-variable-latin.woff2') }}" as="font" type="font/woff2" crossorigin="">
 
-    <!-- Font icons -->
+  
     <link rel="preload" href="{{ asset('assets/icons/finder-icons.woff2') }}" as="font" type="font/woff2" crossorigin="">
     <link rel="stylesheet" href="{{ asset('assets/icons/finder-icons.min.css') }}">
 
-    <!-- Vendor styles -->
     <link rel="stylesheet" href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendor/choices.js/choices.min.css') }}">
 
-    <!-- Bootstrap + Theme styles -->
     <link rel="preload" href="{{ asset('assets/css/theme.min.css') }}" as="style">
     <link rel="preload" href="{{asset('assets/css/theme.rtl.min.css')}}" as="style">
     <link rel="stylesheet" href="{{ asset('assets/css/theme.min.css') }}" id="theme-styles">
 
-    <!-- Customizer -->
     <script src="{{ asset('assets/js/customizer.min.js') }}"></script>
   </head>
 
@@ -1470,15 +1462,11 @@
       </a>
     </div>
 
-
-    <!-- Vendor scripts -->
     <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
     <script src="{{ asset('assets/vendor/choices.js/choices.min.js') }}"></script>
-
-    <!-- Bootstrap + Theme scripts -->
     <script src="{{ asset('assets/js/theme.min.js') }}"></script>
   
 
 </body>
-<!-- Mirrored from finder-html.createx.studio/home-city-guide.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 21 May 2025 06:23:30 GMT -->
+
 </html>
