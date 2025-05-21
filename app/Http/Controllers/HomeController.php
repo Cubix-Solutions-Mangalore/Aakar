b4abb4df240db493913f7809ca4630a6cce8fe29
darshan45672 <drshnbhandary@gmail.com>
2025-05-21 16:00:51 +0530

    done home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Accomodation;
+use App\Models\Entertainment;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $accomodations = Accomodation::with('place','category')->latest()->take(6)->get();
+        $activities = Entertainment::with('place','category')->latest()->take(6)->get();
+
+        // dd($activities);
+        return view('home', compact('accomodations', 'activities'));
     }
 
     public function accomodation()
